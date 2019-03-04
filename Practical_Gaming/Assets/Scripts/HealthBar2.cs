37a63ae3e8b1e2bab249d3613b8a904ed5a70fff
Liam Dowling <liamcamerondowling7@gmail.com>
2019-03-04 21:42:42 +0000

    Player 2 Health Bar
    + Created and assigned the code for the player 2s health bar
    - Need to do the zone/third attack. NEEDS TO BE INSTANCIATED.

---
+++ Practical_Gaming/Assets/Scripts/HealthBar2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar2 : MonoBehaviour
+{
+    public Image currentHealthBar;
+    public Text ratioText;
+
+    private float hitpoint = 150;
+    private float maxHitpoint = 150;
+
+    private void Start()
+    {
+        UpdateHealthBar2();
+    }
+
+    private void UpdateHealthBar2()
+    {
+        float ratio = hitpoint / maxHitpoint; //returns a value between 0 + 1 as hitpoint cannot exceed maxHitPoint
+        currentHealthBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratioText.text = (ratio * 100).ToString("0") + '%';
+    }
+
+    private void TakeDamage(float damage)
+    {
+        hitpoint -= damage;
+        if (hitpoint < 0)
+        {
+            hitpoint = 0;
+            Debug.Log("Dead!");
+        }
+
+        UpdateHealthBar2();
+    }
+
+    private void HealDamage(float heal)
+    {
+        hitpoint += heal;
+        if (hitpoint > maxHitpoint)
+        {
+            hitpoint = maxHitpoint;
+        }
+
+        UpdateHealthBar2();
+    }
+}
